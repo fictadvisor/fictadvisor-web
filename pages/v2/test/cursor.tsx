a8ba6c9af5d98d7d821795e7917c0541ee5e90b7
igaryakqwe <igor.novykov@gmail.com>
2023-01-28 23:06:37 +0200

    add new cursors

---
+++ pages/v2/test/cursor.tsx
@@ -0,0 +1,11 @@
+
+function cursor(){
+  return(
+    <div className="cursor">
+      <div className="cursor-auto">1</div>
+      <div className="cursor-pointer">1</div>
+    </div>
+  );
+}
+
+export default cursor;
