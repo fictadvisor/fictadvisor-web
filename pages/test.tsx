6ada2ba61b2ac20f3956c374a947b19edf5984cd
Beginer101 <106706796+Beginer101@users.noreply.github.com>
2023-02-01 12:38:02 +0200

    added breadcrumb

--- pages/test.tsx
+++ pages/test.tsx
@@ -66,6 +66,39 @@ function test(){
         <div className="tooltip-body">
             <span className="tooltip-text" id="tooltip-text-right">Tooltip Example</span>
         </div>
+
+    <div id="breadcrumb">
+
+        <div className="icon-home-breadcrumb">
+                <svg width="10.5" height="10.5" viewBox="0 0 10.5 10.5" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M2 16l3.111-3.111M5.111 12.889l10.889-10.889 10.889 10.889M5.111 12.889v15.556c0 0.413 0.164 0.808 0.456 1.1s0.687 0.456 1.1 0.456h4.667M26.889 12.889l3.111 3.111M26.889 12.889v15.556c0 0.413-0.164 0.808-0.456 1.1s-0.687 0.456-1.1 0.456h-4.667M11.333 30c0.413 0 0.808-0.164 1.1-0.456s0.456-0.687 0.456-1.1v-6.222c0-0.413 0.164-0.808 0.456-1.1s0.687-0.456 1.1-0.456h3.111c0.413 0 0.808 0.164 1.1 0.456s0.456 0.687 0.456 1.1v6.222c0 0.413 0.164 0.808 0.456 1.1s0.687 0.456 1.1 0.456M11.333 30h9.333" stroke="#d4d4d4" stroke-width="3.1111" stroke-linecap="round" stroke-linejoin="round"/>
+                </svg>
+        </div>
+
+        <div className="home-breadcrumb">
+            <a href="">Home</a>
+        </div>
+
+        <div className="chevron-right-breadcrumb">
+                <svg width="4" height="8" viewBox="0 0 4 8" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M3.2 3.2l12.8 12.8-12.8 12.8" stroke="#d4d4d4" stroke-width="1.92" stroke-linecap="round" stroke-linejoin="round"/>
+                </svg>
+        </div>
+
+        <div className="h-breadcrumb">
+            <a href="">h</a>
+        </div>
+
+        <div className="chevron-right-breadcrumb">
+                <svg width="4" height="8" viewBox="0 0 4 8" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M3.2 3.2l12.8 12.8-12.8 12.8" stroke="#d4d4d4" stroke-width="1.92" stroke-linecap="round" stroke-linejoin="round"/>
+                </svg>
+        </div>
+
+        <div className="breadcrumb">
+            <a href="">Breadcrumb</a>
+        </div>
+    </div>
     </div>
     </div>
         );
