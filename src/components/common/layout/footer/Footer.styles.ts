b251177aeee2a3da193e0c6f5eb9bcd7b3026faa
Markusplay <mshilo2004@gmail.com>
2023-03-28 17:53:34 +0300

    fix: changed html attributes to mui components

---
+++ src/components/common/layout/footer/Footer.styles.ts
@@ -0,0 +1,92 @@
+import { SxProps, Theme } from '@mui/material/styles';
+
+export const footerContainer: SxProps<Theme> = {
+  display: 'grid',
+  overflow: 'hidden',
+  maxWidth: '100%',
+  height: '300px',
+  bottom: 0,
+  minWidth: 'fit-content',
+  gridTemplateColumns: '3fr 1fr 1fr 1fr',
+  backgroundColor: 'rgba(23, 23, 23, 0.62)',
+};
+
+export const footerLogoContainer: SxProps<Theme> = {
+  width: '100%',
+  marginTop: '40px',
+  marginLeft: '8.4%',
+};
+
+export const footerLogo: SxProps<Theme> = {
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'flex-start',
+  marginLeft: 'unset',
+  width: '197px',
+  paddingLeft: '0px',
+  paddingRight: '0px',
+  height: '28px',
+  '& img': {
+    height: '20px',
+  },
+};
+export const signature: SxProps<Theme> = {
+  marginTop: '8px',
+  fontWeight: 400,
+  fontSize: '16px',
+  lineHeight: '24px',
+  color: '#FFFFFF',
+};
+export const mainReferences: SxProps<Theme> = {
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'flex-start',
+  gap: '2px',
+  height: '220px',
+  marginTop: '40px',
+  marginLeft: '20%',
+};
+
+export const title: SxProps<Theme> = {
+  display: 'flex',
+  justifyContent: 'left',
+  padding: '2px 12px',
+  // width: '187px',
+  whiteSpace: 'nowrap',
+  height: '30px',
+  '& p': {
+    fontWeight: 400,
+    fontSize: '16px',
+    lineHeight: '150%',
+    color: '#A3A3A3',
+  },
+};
+
+export const support: SxProps<Theme> = {
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'flex-start',
+  marginTop: '40px',
+  marginLeft: '15%',
+  height: '106px',
+  gap: '2px',
+};
+export const socialMedia: SxProps<Theme> = {
+  display: 'flex',
+  justifyContent: 'center',
+  flexDirection: 'column',
+  alignItems: 'flex-start',
+  // flexDirection: 'column',
+  marginTop: '40px',
+  height: 'fit-content',
+  gap: '4px',
+};
+
+export const socialButtons: SxProps<Theme> = {
+  display: 'flex',
+  justifyContent: 'center',
+  flexDirection: 'column',
+  alignItems: 'flex-start',
+  height: 'fit-content',
+  gap: '2px',
+};
--- src/components/common/layout/footer/Footer.tsx
+++ src/components/common/layout/footer/Footer.tsx
@@ -1,16 +1,17 @@
 import React from 'react';
+import { Container, Grid, Typography } from '@mui/material';
 import Link from 'next/link';
 
 import { GitHubIcon } from '@/components/common/custom-svg/GitHub';
 import { InstagramIcon } from '@/components/common/custom-svg/Instagram';
 import { TelegramIcon } from '@/components/common/custom-svg/Telegram';
-import Button, {
-  ButtonSize,
-  ButtonVariant,
-} from '@/components/common/ui/button';
 import useIsMobile from '@/hooks/use-is-mobile/UseIsMobile';
 
-import styles from './Footer.module.scss';
+import Button, { ButtonSize, ButtonVariant } from '../../ui/button/Button';
+
+import * as styles from './Footer.styles';
+
+// import styles from './Footer.module.scss';
 
 const Footer: React.FC = () => {
   const isMobile = useIsMobile(768);
@@ -51,129 +52,119 @@ const Footer: React.FC = () => {
       ];
 
   return (
-    <div className={styles['footer-container']}>
-      <div className={styles['footer-logo-container']}>
-        <div className={styles['footer-logo']}>
+    <Grid container sx={styles.footerContainer}>
+      <Grid item sx={styles.footerLogoContainer}>
+        <Container sx={styles.footerLogo}>
           <Link href={'/'}>
             <img src={`/assets/logo.png`} alt="logo" />
           </Link>
-        </div>
-
-        <div className={styles['signature']}>
-          <p>By Dev-відділ СР ФІОТ</p>
-        </div>
-      </div>
-      <div className={styles['main-references']}>
-        <div className={styles['title']}>
-          <p>Основні посилання</p>
-        </div>
-        <Link href={'/'} style={{ height: '36px' }}>
-          <Button
-            style={{ paddingLeft: '12px' }}
-            text="Головна"
-            size={ButtonSize.SMALL}
-            variant={ButtonVariant.TEXT}
-          />
-        </Link>
-
-        <Link href={'/poll'} style={{ height: '36px' }}>
-          <Button
-            style={{ paddingLeft: '12px' }}
-            text="Опитування"
-            size={ButtonSize.SMALL}
-            variant={ButtonVariant.TEXT}
-          />
-        </Link>
-        <Link href={'/teachers'} style={{ height: '36px' }}>
-          <Button
-            style={{ paddingLeft: '12px' }}
-            text="Викладачі"
-            size={ButtonSize.SMALL}
-            variant={ButtonVariant.TEXT}
-          />
-        </Link>
-        <Link href={'/subjects'} style={{ height: '36px' }}>
-          <Button
-            style={{ paddingLeft: '12px' }}
-            // style={{ height: '36px', padding: '8px 12px' }}
-            text="Предмети"
-            size={ButtonSize.SMALL}
-            variant={ButtonVariant.TEXT}
-          />
-        </Link>
-        {/* <Link href={{}} style={{ height: '36px' }}>
-          <Button
-            text="Розклад"
-            size={ButtonSize.SMALL}
-            variant={ButtonVariant.TEXT}
-          />
-        </Link> */}
-      </div>
-      <div className={styles['support']}>
-        <div className={styles['title']}>
-          <p>Підтримка</p>
-        </div>
-        <Link href={'/privacy'} style={{ height: '36px' }}>
-          <Button
-            style={{ paddingLeft: '12px' }}
-            text="Конфіденційність"
-            size={ButtonSize.SMALL}
-            variant={ButtonVariant.TEXT}
-          />
-        </Link>
-        <Link href={'https://t.me/fict_robot'} style={{ height: '36px' }}>
-          <Button
-            style={{ paddingLeft: '12px' }}
-            text="FICT robot"
-            size={ButtonSize.SMALL}
-            variant={ButtonVariant.TEXT}
-          />
-        </Link>
-      </div>
-      <div className={styles['social-media']}>
-        <div className={styles['title']}>
-          <p>Соцмережі</p>
-        </div>
-        <div className={styles['social-buttons']}>
-          <Link
-            style={{ height: '36px', width: '36px' }}
-            href={socialLabels[0].url}
-          >
+        </Container>
+        <Typography sx={styles.signature}>By Dev-відділ СР ФІОТ</Typography>
+      </Grid>
+      <Grid item>
+        <Container sx={styles.mainReferences}>
+          <Container sx={styles.title}>
+            <p>Основні посилання</p>
+          </Container>
+          <Link href={'/'} style={{ height: '36px' }}>
             <Button
               style={{ paddingLeft: '12px' }}
-              text={socialLabels[0].text}
-              startIcon={socialLabels[0].icon}
+              text="Головна"
               size={ButtonSize.SMALL}
               variant={ButtonVariant.TEXT}
             />
           </Link>
-          <Link
-            href={socialLabels[1].url}
-            style={{ height: '36px', width: '36px' }}
-          >
+          <Link href={'/poll'} style={{ height: '36px' }}>
             <Button
               style={{ paddingLeft: '12px' }}
-              text={socialLabels[1].text}
-              startIcon={socialLabels[1].icon}
+              text="Опитування"
               size={ButtonSize.SMALL}
               variant={ButtonVariant.TEXT}
             />
           </Link>
-          <Link
-            href={socialLabels[2].url}
-            style={{ height: '36px', width: '36px' }}
-          >
+          <Link href={'/teachers'} style={{ height: '36px' }}>
             <Button
               style={{ paddingLeft: '12px' }}
-              text={socialLabels[2].text}
-              startIcon={socialLabels[2].icon}
+              text="Викладачі"
               size={ButtonSize.SMALL}
               variant={ButtonVariant.TEXT}
             />
           </Link>
-        </div>
-      </div>
-    </div>
+          <Link href={'/subjects'} style={{ height: '36px' }}>
+            <Button
+              style={{ paddingLeft: '12px' }}
+              text="Предмети"
+              size={ButtonSize.SMALL}
+              variant={ButtonVariant.TEXT}
+            />
+          </Link>
+        </Container>
+        <Container sx={styles.support}>
+          <Container sx={styles.title}>
+            <p>Підтримка</p>
+          </Container>
+          <Link href={'/privacy'} style={{ height: '36px' }}>
+            <Button
+              style={{ paddingLeft: '12px' }}
+              text="Конфіденційність"
+              size={ButtonSize.SMALL}
+              variant={ButtonVariant.TEXT}
+            />
+          </Link>
+          <Link href={'https://t.me/fict_robot'} style={{ height: '36px' }}>
+            <Button
+              style={{ paddingLeft: '12px' }}
+              text="FICT robot"
+              size={ButtonSize.SMALL}
+              variant={ButtonVariant.TEXT}
+            />
+          </Link>
+        </Container>
+        <Container sx={styles.socialMedia}>
+          <Container sx={styles.title}>
+            <p>Соцмережі</p>
+          </Container>
+          <Container sx={styles.socialButtons}>
+            <Link
+              style={{ height: '36px', width: '36px' }}
+              href={socialLabels[0].url}
+            >
+              <Button
+                style={{ paddingLeft: '12px' }}
+                text={socialLabels[0].text}
+                startIcon={socialLabels[0].icon}
+                size={ButtonSize.SMALL}
+                variant={ButtonVariant.TEXT}
+              />
+            </Link>
+            <Link
+              href={socialLabels[1].url}
+              style={{ height: '36px', width: '36px' }}
+            >
+              <Button
+                style={{ paddingLeft: '12px' }}
+                text={socialLabels[1].text}
+                startIcon={socialLabels[1].icon}
+                size={ButtonSize.SMALL}
+                variant={ButtonVariant.TEXT}
+              />
+            </Link>
+            <Link
+              href={socialLabels[2].url}
+              style={{ height: '36px', width: '36px' }}
+            >
+              <Button
+                style={{ paddingLeft: '12px' }}
+                text={socialLabels[2].text}
+                startIcon={socialLabels[2].icon}
+                size={ButtonSize.SMALL}
+                variant={ButtonVariant.TEXT}
+              />
+            </Link>
+          </Container>{' '}
+        </Container>
+      </Grid>
+    </Grid>
   );
 };
 
