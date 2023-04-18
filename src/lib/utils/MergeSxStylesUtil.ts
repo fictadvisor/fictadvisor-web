c3483faf8dcfa40832eb1819b26d247ab6fb521d
ria4232 <ipkovalenomaria@gmail.com>
2023-04-18 19:36:33 +0300

    fix: fixed merging styles

--- src/lib/utils/MergeSxStylesUtil.ts
+++ src/lib/utils/MergeSxStylesUtil.ts
@@ -1,13 +1,13 @@
 import { SxProps, Theme } from '@mui/material/styles';
 
 export const withSxProp = (
-  sxProp: SxProps<Theme> | undefined,
   other: SxProps<Theme>,
+  sxProp: SxProps<Theme> | undefined,
 ): SxProps<Theme> => {
   if (!sxProp) {
     return other;
   } else {
-    return mergeSx(sxProp, other);
+    return mergeSx(other, sxProp);
   }
 };
 //used while merging sx from props and inner styles
