e1b43428ef4ffa788ec1460e2c6d15ebcc47bda4
HJK <supermaster98z@gmail.com>
2023-05-11 11:13:18 +0100

    fix: added more variations in LineGraphPage.tsx

--- src/components/pages/test-pages/line-graph-page/LineGraphPage.tsx
+++ src/components/pages/test-pages/line-graph-page/LineGraphPage.tsx
@@ -6,12 +6,16 @@ import * as styles from './LineGraph.styles';
 
 const LineGraphPage = () => (
   <Box sx={styles.wrapper}>
-    <LineGraph value={35} label="1. Доступність оцінок" />
-    <LineGraph value={61} label="2. Комунікативність" />
-    <LineGraph value={50} label="3. Якась еще хуйня" />
-    <LineGraph value={76} label="4. І ЕЩЕ ЯКАСЬ ХУЙНЯ" />
-    <LineGraph value={100} label="5. НУ І ЕЩЕ ЯКАСЬ ХУЙНЯ" />
-    <LineGraph value={20} label="6. АХАХХАХАХ" />
+    <LineGraph value={10} label="1. Доступність оцінок" />
+    <LineGraph value={20} label="2. Комунікативність" />
+    <LineGraph value={30} label="3. Якась еще хуйня" />
+    <LineGraph value={40} label="4. І ЕЩЕ ЯКАСЬ ХУЙНЯ" />
+    <LineGraph value={50} label="5. НУ І ЕЩЕ ЯКАСЬ ХУЙНЯ" />
+    <LineGraph value={60} label="6. АХАХХАХАХ" />
+    <LineGraph value={70} label="6. АХАХХАХАХ" />
+    <LineGraph value={80} label="6. АХАХХАХАХ" />
+    <LineGraph value={90} label="6. АХАХХАХАХ" />
+    <LineGraph value={100} label="6. АХАХХАХАХ" />
   </Box>
 );
 
