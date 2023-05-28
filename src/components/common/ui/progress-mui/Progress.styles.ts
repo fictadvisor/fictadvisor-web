5de3d8231a99411d81e0389966d8fdfc4f0f11f5
Yevhenii Mynenko <68693183+Yevheniy2020@users.noreply.github.com>
2023-05-28 20:51:06 +0300

    enchancement: fix review 2

--- src/components/common/ui/progress-mui/Progress.styles.ts
+++ src/components/common/ui/progress-mui/Progress.styles.ts
@@ -1,7 +1,6 @@
 import { SxProps, Theme } from '@mui/material/styles';
 
 export const progressFront: SxProps<Theme> = {
-  position: 'relative',
   '.MuiCircularProgress-svg': {
     strokeLinecap: 'round',
   },
@@ -10,5 +9,4 @@ export const progressFront: SxProps<Theme> = {
 export const progressBack: SxProps<Theme> = {
   color: 'backgroundDark.300',
   position: 'absolute',
-  left: 0,
 };
