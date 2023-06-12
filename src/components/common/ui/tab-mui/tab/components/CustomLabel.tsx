162976ec7f2b4bf5a6b0d6fd74e7f3e6e0b62745
andrewvitrenko <avitrenko2004@gmail.com>
2023-06-12 21:34:13 +0200

    enhancement(`tab-mui`): - moved types to separate folder - added default values in props - fixed styles - added enum for position

--- src/components/common/ui/tab-mui/tab/components/CustomLabel.tsx
+++ src/components/common/ui/tab-mui/tab/components/CustomLabel.tsx
@@ -1,10 +1,10 @@
-import React, { FC } from 'react';
+import { FC, ReactNode } from 'react';
 import Box from '@mui/material/Box';
 
 import * as styles from '../Tab.styles';
 
 interface CustomLabelProps {
-  label: string | React.ReactElement;
+  label: string | ReactNode;
   count: number | string;
   disabled: boolean;
 }
@@ -17,7 +17,7 @@ export const CustomLabel: FC<CustomLabelProps> = ({
   return (
     <Box sx={styles.label}>
       {label}
-      {count != null && <Box sx={styles.counter(disabled)}>{count}</Box>}
+      {count !== null && <Box sx={styles.counter(disabled)}>{count}</Box>}
     </Box>
   );
 };
---
+++ src/components/common/ui/tag-mui/types/index.ts
@@ -0,0 +1,34 @@
+import { ReactNode } from 'react';
+import { SxProps, Theme } from '@mui/material/styles';
+
+export enum TagColor {
+  PRIMARY = 'primary',
+  SUCCESS = 'success',
+  ERROR = 'error',
+  WARNING = 'warning',
+  INFO = 'info',
+  SECONDARY = 'secondary',
+  VIOLET = 'violet',
+  MINT = 'mint',
+  ORANGE = 'orange',
+}
+
+export enum TagSize {
+  MEDIUM = 'medium',
+  SMALL = 'small',
+}
+
+export enum TagVariant {
+  FILL = 'fill',
+  OUTLINE = 'outline',
+  DARKER = 'darker',
+}
+
+export interface TagProps {
+  text: string;
+  size?: TagSize;
+  color?: TagColor;
+  variant?: TagVariant;
+  icon?: ReactNode;
+  sx?: SxProps<Theme>;
+}
