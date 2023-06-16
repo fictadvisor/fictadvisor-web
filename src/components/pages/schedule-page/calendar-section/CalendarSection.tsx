bdea7c508d4dc5eaeb0eeec6aa35045465e0300b
kujo205 <kuc8301@gmail.com>
2023-06-16 21:49:55 +0300

    button added

--- src/components/pages/schedule-page/calendar-section/CalendarSection.tsx
+++ src/components/pages/schedule-page/calendar-section/CalendarSection.tsx
@@ -1,8 +1,20 @@
 import type { FC } from 'react';
+import { PlusIcon } from '@heroicons/react/24/outline';
+
+import Button from '@/components/common/ui/button-mui/Button';
 
 import styles from './calendar-section.module.scss';
 export const CalendarSection: FC<any> = ({}) => {
   return (
-    <section className={styles['calendar-section']}>I AM CALENDAR</section>
+    <section className={styles['calendar-section']}>
+      <Button
+        sx={{ maxWidth: '350px' }}
+        text={'Додати подію'}
+        // color = 'primary',
+        variant={'outline'}
+        // size="large"
+        startIcon={<PlusIcon />}
+      />
+    </section>
   );
 };
