1f0e81dec9ec7d80c6125dc039439333fcb7dfe3
kujo205 <kuc8301@gmail.com>
2023-06-16 21:49:55 +0300

    button added

--- src/components/pages/schedule-page/calendar-section/CalendarSection.tsx
+++ src/components/pages/schedule-page/calendar-section/CalendarSection.tsx
@@ -1,46 +1,20 @@
 import type { FC } from 'react';
 import { PlusIcon } from '@heroicons/react/24/outline';
-import { Box, Stack } from '@mui/material';
-import { Form, Formik } from 'formik';
 
 import Button from '@/components/common/ui/button-mui/Button';
-import {
-  ButtonSize,
-  ButtonVariant,
-} from '@/components/common/ui/button-mui/types';
 
-import { CheckBoxSection } from './components/checkboxes-section/CheckBoxSection';
-import { DatePicker } from './components/date-picker/DatePicker';
-import { TabSection } from './components/tabs-section/TabSection';
-import * as styles from './CalendarSection.styles';
-
-export const CalendarSection: FC<never> = () => {
+import styles from './calendar-section.module.scss';
+export const CalendarSection: FC<any> = ({}) => {
   return (
-    <Box sx={styles.mainWrapper}>
-      <Formik
-        validateOnMount
-        validateOnChange
-        initialValues={{}}
-        onSubmit={values => {
-          console.log(values);
-        }}
-      >
-        <Box sx={styles.sticky}>
-          <Form>
-            <Stack sx={styles.wrapper}>
-              <Button
-                text={'Додати подію'}
-                variant={ButtonVariant.OUTLINE}
-                startIcon={<PlusIcon />}
-                size={ButtonSize.SMALL}
-              />
-              <TabSection />
-              <DatePicker />
-              <CheckBoxSection />
-            </Stack>
-          </Form>
-        </Box>
-      </Formik>
-    </Box>
+    <section className={styles['calendar-section']}>
+      <Button
+        sx={{ maxWidth: '350px' }}
+        text={'Додати подію'}
+        // color = 'primary',
+        variant={'outline'}
+        // size="large"
+        startIcon={<PlusIcon />}
+      />
+    </section>
   );
 };
