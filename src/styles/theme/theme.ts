500e14c2625d82d10433fe263f59bb0de6f21776
kujo205 <kuc8301@gmail.com>
2023-06-17 17:45:29 +0300

    added ukrainian locale to the theme

--- src/styles/theme/theme.ts
+++ src/styles/theme/theme.ts
@@ -1,13 +1,17 @@
 import { createTheme } from '@mui/material/styles';
+import { ukUA } from '@mui/x-date-pickers/locales';
 
 import customBreakpoints from '@/styles/theme/constants/breakpoints';
 import customPalette from '@/styles/theme/constants/pallete';
 import customTypography from '@/styles/theme/constants/typography';
 
-const theme = createTheme({
-  palette: customPalette,
-  typography: customTypography,
-  breakpoints: customBreakpoints,
-});
+const theme = createTheme(
+  {
+    palette: customPalette,
+    typography: customTypography,
+    breakpoints: customBreakpoints,
+  },
+  ukUA,
+);
 
 export default theme;
