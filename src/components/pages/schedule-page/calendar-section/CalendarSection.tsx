fc8e1e75fb42038606b8a164268d2ba0486d9197
kujo205 <kuc8301@gmail.com>
2023-06-17 16:31:20 +0300

    added dropdown, tabs and button

--- src/components/pages/schedule-page/calendar-section/CalendarSection.tsx
+++ src/components/pages/schedule-page/calendar-section/CalendarSection.tsx
@@ -1,20 +1,52 @@
 import type { FC } from 'react';
 import { PlusIcon } from '@heroicons/react/24/outline';
+import { Form, Formik } from 'formik';
 
 import Button from '@/components/common/ui/button-mui/Button';
+import { Dropdown } from '@/components/common/ui/form/dropdown-mui/Dropdown';
+
+import { DatePicker } from './date-picker/DatePicker';
+import { TabSection } from './tabs-section/TabSection';
 
 import styles from './calendar-section.module.scss';
 export const CalendarSection = ({}) => {
   return (
     <section className={styles['calendar-section']}>
       <Button
-        sx={{ maxWidth: '350px' }}
+        sx={{ maxWidth: '330px' }}
         text={'Додати подію'}
-        // color = 'primary',
         variant={'outline'}
-        // size="large"
         startIcon={<PlusIcon />}
       />
+      <Formik
+        validateOnMount
+        validateOnChange
+        initialValues={{}}
+        onSubmit={values => {
+          console.log(values);
+        }}
+      >
+        <Form
+          style={{
+            width: '90%',
+            display: 'flex',
+            gap: '24px',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+          <TabSection />
+          <Dropdown
+            width={'min(300px,100%)'}
+            placeholder="NO LABEL MFCKRS"
+            options={[{ label: 'l1', id: 'id1' }]}
+            name="teacherId1"
+            showRemark
+            label="Група"
+          />
+          <DatePicker />
+        </Form>
+      </Formik>
     </section>
   );
 };
---
+++ src/components/pages/schedule-page/calendar-section/date-picker/DatePicker.tsx
@@ -0,0 +1,5 @@
+import { FC } from 'react';
+
+export const DatePicker: FC<unknown> = () => {
+  return <div />;
+};
