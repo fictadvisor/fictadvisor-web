a62c39b3ff2b36b4df21dc50294dcc3f3499a85a
Yevhenii Mynenko <68693183+Yevheniy2020@users.noreply.github.com>
2023-06-19 17:17:11 +0300

    enhancement: replace legacy Tab component with MUI version (1 page)

--- src/components/pages/account-page/AccountPage.tsx
+++ src/components/pages/account-page/AccountPage.tsx
@@ -5,23 +5,22 @@ import {
   LockClosedIcon,
   UsersIcon,
 } from '@heroicons/react/24/outline';
+import { Box } from '@mui/material';
 import { useRouter } from 'next/router';
 
 import Breadcrumbs from '@/components/common/ui/breadcrumbs';
-import {
-  TabItem,
-  TabItemContentPosition,
-  TabList,
-  TabPanel,
-  TabPanelsList,
-} from '@/components/common/ui/tab';
-import { TabItemContentSize } from '@/components/common/ui/tab/tab-item/TabItem';
+import Tab from '@/components/common/ui/tab-mui/tab';
+import TabContext from '@/components/common/ui/tab-mui/tab-context';
+import TabList from '@/components/common/ui/tab-mui/tab-list';
+import TabPanel from '@/components/common/ui/tab-mui/tab-panel';
 import GeneralTab from '@/components/pages/account-page/components/general-tab';
 import GroupTab from '@/components/pages/account-page/components/group-tab';
 import SecurityTab from '@/components/pages/account-page/components/security-tab';
 import useAuthentication from '@/hooks/use-authentication';
 
 import PageLayout from '../../common/layout/page-layout/PageLayout';
+
+import * as stylesMui from './AccountPage.styles';
 
 import styles from './AccountPage.module.scss';
 
@@ -85,67 +84,55 @@ const AccountPage = () => {
           ]}
         />
       </div>
-      <div className={styles['tabs-content']}>
-        <TabList
-          className={styles['tab-list']}
-          onChange={async value => {
-            await push({ query: { ...query, tab: value } }, undefined, {
-              shallow: true,
-            });
-            setIndex(value as AccountPageTabs);
-          }}
-          currentValue={index}
-        >
-          <TabItem
-            size={TabItemContentSize.NORMAL}
-            text="Загальне"
-            position={TabItemContentPosition.LEFT}
-            icon={<AcademicCapIcon className="icon" />}
-            value={AccountPageTabs.GENERAL}
-          />
-          <TabItem
-            size={TabItemContentSize.NORMAL}
-            text="Безпека"
-            position={TabItemContentPosition.LEFT}
-            icon={<LockClosedIcon className="icon" />}
-            value={AccountPageTabs.SECURITY}
-          />
-          <TabItem
-            size={TabItemContentSize.NORMAL}
-            text="Група"
-            position={TabItemContentPosition.LEFT}
-            icon={<UsersIcon className="icon" />}
-            value={AccountPageTabs.GROUP}
-          />
-        </TabList>
-        {isLoggedIn && (
-          <TabPanelsList
-            className={styles['tab-panels-list']}
-            currentValue={index}
+      <Box sx={stylesMui.tabContext}>
+        <TabContext value={index}>
+          <TabList
+            onChange={async (event, value) => {
+              await push({ query: { ...query, tab: value } }, undefined, {
+                shallow: true,
+              });
+              setIndex(value as AccountPageTabs);
+            }}
+            aria-label="lab API tabs example"
+            sx={stylesMui.tabList}
           >
-            <>
-              <TabPanel
-                className={styles['tab-panel']}
-                value={AccountPageTabs.GENERAL}
-              >
+            <Tab
+              label="Загальне"
+              value={AccountPageTabs.GENERAL}
+              icon={<AcademicCapIcon className="icon" />}
+              textPosition="left"
+            />
+            <Tab
+              label="Безпека"
+              value={AccountPageTabs.SECURITY}
+              icon={<LockClosedIcon className="icon" />}
+              textPosition="left"
+            />
+            <Tab
+              label="Група"
+              value={AccountPageTabs.GROUP}
+              icon={<UsersIcon className="icon" />}
+              textPosition="left"
+            />
+          </TabList>
+          {isLoggedIn && (
+            <Box sx={stylesMui.tabPanelsList}>
+              <TabPanel sx={stylesMui.tabPanel} value={AccountPageTabs.GENERAL}>
                 <GeneralTab />
               </TabPanel>
               <TabPanel
-                className={styles['tab-panel']}
+                sx={stylesMui.tabPanel}
                 value={AccountPageTabs.SECURITY}
               >
                 <SecurityTab />
               </TabPanel>
-              <TabPanel
-                className={styles['tab-panel']}
-                value={AccountPageTabs.GROUP}
-              >
+              <TabPanel sx={stylesMui.tabPanel} value={AccountPageTabs.GROUP}>
                 <GroupTab />
               </TabPanel>
-            </>
-          </TabPanelsList>
-        )}
-      </div>
+            </Box>
+          )}
+        </TabContext>
+      </Box>
     </PageLayout>
   );
 };
