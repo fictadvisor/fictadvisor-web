c8c4e05358dfa3bfe542cae900f43748a13508a7
Markusplay <mshilo2004@gmail.com>
2023-06-23 17:03:58 +0300

    hot fix

--- src/components/pages/main-page/MainPage.tsx
+++ src/components/pages/main-page/MainPage.tsx
@@ -1,5 +1,4 @@
-import React from 'react';
-import { useQuery } from 'react-query';
+import { FC } from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
