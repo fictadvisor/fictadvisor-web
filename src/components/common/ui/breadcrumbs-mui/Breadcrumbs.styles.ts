43441b045d46a7ab3453bbfcfdc24a1277b5d7ba
miss-linetta <natashapetrovych@gmail.com>
2023-06-25 19:53:05 +0300

    feature: changed styles

--- src/components/common/ui/breadcrumbs-mui/Breadcrumbs.styles.ts
+++ src/components/common/ui/breadcrumbs-mui/Breadcrumbs.styles.ts
@@ -1,9 +1,11 @@
 import { SxProps, Theme } from '@mui/system';
 
+import palette from '@/styles/theme/constants/pallete/palette';
+import typography from '@/styles/theme/constants/typography/typography';
+
 export const homeIcon: SxProps<Theme> = {
   width: '14px',
   height: '14px',
-  position: 'relative',
   transform: 'translateY(-1px)',
 };
 
@@ -17,12 +19,11 @@ export const breadcrumb: SxProps<Theme> = {
     },
   },
   a: {
-    fontSize: '14px',
+    fontSize: typography.body1,
     gap: '4px',
     display: 'flex',
-    flexDirection: 'row',
     alignItems: 'center',
-    color: '#E5E5E5',
+    color: palette.grey[700],
     span: {
       overflow: 'hidden',
       textOverflow: 'ellipsis',
