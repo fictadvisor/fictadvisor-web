54380369f7d5bb50cadac9b90b7d3bfc746a2eb1
miss-linetta <natashapetrovych@gmail.com>
2023-07-16 17:31:55 +0300

    feature: added text aria

---
+++ src/components/pages/test-pages/text-aria/TextAria.styles.ts
@@ -0,0 +1,30 @@
+import { SxProps, Theme } from '@mui/material/styles';
+
+import { TextAriaSize } from '@/components/pages/test-pages/text-aria/types';
+
+export const textAria = (size: TextAriaSize): SxProps<Theme> => ({
+  backgroundColor: 'backgroundDark.200',
+  borderRadius: '8px',
+  '& div': {
+    color: 'grey.500',
+  },
+  '&:hover': {
+    div: {
+      color: 'grey.700',
+    },
+  },
+  ...(size === TextAriaSize.SMALL && {
+    width: '308px',
+    div: {
+      padding: '12px',
+      fontSize: 'body1.fontSize',
+    },
+  }),
+  ...(size === TextAriaSize.NORMAL && {
+    width: '480px',
+    div: {
+      padding: '16px 16px 16px 20px',
+      fontSize: 'body2.fontSize',
+    },
+  }),
+});
---
+++ src/components/pages/test-pages/text-aria/types/index.ts
@@ -0,0 +1,4 @@
+export enum TextAriaSize {
+  NORMAL = 'normal',
+  SMALL = 'small',
+}
