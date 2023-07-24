0d94507da4ef113cfaee3f8e39e9c38cc1596b2a
Yevhenii Mynenko <68693183+Yevheniy2020@users.noreply.github.com>
2023-07-24 13:58:07 +0300

    enhancement: changed type img

--- src/components/pages/main-page/components/resource-card/ResourceCard.tsx
+++ src/components/pages/main-page/components/resource-card/ResourceCard.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Box, Typography } from '@mui/material';
-import Image from 'next/image';
 import Link from 'next/link';
 
 import * as styles from './ResourceCard.styles';
@@ -19,13 +18,7 @@ const ResourceCard: React.FC<CardProps> = ({
     <Link href={href}>
       <Box sx={styles.card}>
         <Box sx={styles.cardContent}>
-          <Image
-            width={100}
-            height={100}
-            quality={100}
-            src={image}
-            alt="student resource"
-          />
+          <img src={image} alt="student resource" />
           <Typography sx={styles.text}>{text}</Typography>
         </Box>
       </Box>
