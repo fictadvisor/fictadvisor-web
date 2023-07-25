0ef6c831d550d154371673ff7cdb6250f4750a28
kujo205 <kuc8301@gmail.com>
2023-07-25 15:22:39 +0300

    added text-area

--- src/components/pages/test-pages/TextAriaPage.tsx
+++ src/components/pages/test-pages/TextAriaPage.tsx
@@ -1,15 +1,15 @@
 import { Box } from '@mui/material';
 
-import TextAria from '@/components/common/ui/test/text-aria';
-import { TextAriaSize } from '@/components/common/ui/test/text-aria/types';
+import TextAria from '@/components/pages/schedule-page/schedule-event-edit-section/components/text-area';
+import { TextAriaSize } from '@/components/pages/schedule-page/schedule-event-edit-section/components/text-area/types';
 
 import * as styles from './TextAriaPage.styles';
 
 const TextAriaPage = () => {
   return (
     <Box sx={styles.container}>
-      <TextAria value={'Text Aria'} size={TextAriaSize.NORMAL} />
-      <TextAria value={'Text Aria'} size={TextAriaSize.SMALL} />
+      <TextAria placeholder={'Text Aria'} size={TextAriaSize.NORMAL} />
+      <TextAria placeholder={'Text Aria'} size={TextAriaSize.SMALL} />
     </Box>
   );
 };
