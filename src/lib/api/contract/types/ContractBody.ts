f65a46e94e5e5a9b518eff745933915091f991fb
Markusplay <mshilo2004@gmail.com>
2023-07-28 00:13:40 +0300

    added ContractAPI

---
+++ src/lib/api/contract/types/ContractBody.ts
@@ -0,0 +1,43 @@
+export enum StudyTypeParam {
+  CONTRACT = 'Контракт',
+  BUDGET = 'Бюджет',
+}
+
+export enum StudyFormParam {
+  FULL_TIME = 'Денна',
+  PART_TIME = 'Заочна',
+}
+
+export enum PaymentTypeParam {
+  EVERY_QUARTER = 'Щоквартально',
+  EVERY_SEMESTER = 'Щосеместрово',
+}
+
+interface PersonalData {
+  firstName: string;
+  middleName?: string;
+  lastName: string;
+  passportSeries?: string;
+  passportNumber: string;
+  passportInstitute: string;
+  passportDate: string;
+  address: string;
+  settlement: string;
+  idCode?: string;
+  phoneNumber: string;
+  email: string;
+}
+
+interface MetaContract {
+  speciality: string;
+  studyType: StudyTypeParam;
+  studyForm: StudyFormParam;
+  paymentType?: PaymentTypeParam;
+  isToAdmission: boolean;
+}
+
+export interface ContractBody {
+  entrant: PersonalData;
+  representative?: PersonalData;
+  meta: MetaContract;
+}
