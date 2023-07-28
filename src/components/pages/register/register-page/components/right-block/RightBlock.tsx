e2f92cfa6e938244d07391ea919c6cb9b0d2b1d0
Yevhenii Mynenko <68693183+Yevheniy2020@users.noreply.github.com>
2023-07-28 03:10:38 +0300

    enhancement: rewrote right block in register page
    -rewrote right block`s styles
    -rewrote right block`s file

--- src/components/pages/register/register-page/components/right-block/RightBlock.tsx
+++ src/components/pages/register/register-page/components/right-block/RightBlock.tsx
@@ -1,8 +1,11 @@
+import { Box, Typography } from '@mui/material';
 import Image from 'next/image';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
 
-import Button from '@/components/common/ui/button';
+import Button from '@/components/common/ui/button-mui';
+
+import * as stylesMUI from './RightBlock.styles';
 
 import styles from './RightBlock.module.scss';
 
@@ -10,7 +13,7 @@ const RightBlock = () => {
   const router = useRouter();
 
   return (
-    <div className={styles['left-block']}>
+    <Box sx={stylesMUI.rightBlock}>
       <Link href="/">
         <Image
           className={styles['login-logo']}
@@ -21,15 +24,17 @@ const RightBlock = () => {
           height={54}
         />
       </Link>
-      <h3 className={styles['login-text']}>Вже маєш акаунт? Заходь!</h3>
+      <Typography variant="h3" sx={stylesMUI.loginText}>
+        Вже маєш акаунт? Заходь!
+      </Typography>
       <Button
-        className={styles['login-button']}
+        sx={stylesMUI.loginButton}
         text="Вхід"
         onClick={() => {
           void router.push('/login');
         }}
       />
-    </div>
+    </Box>
   );
 };
 
