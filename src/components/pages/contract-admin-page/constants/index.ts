b6ba2e432d264fe250b8c09229183885e1ec67ef
igaryakqwe <igor.novykov@gmail.com>
2023-08-01 02:23:16 +0300

    initial commit

---
+++ src/components/pages/contract-admin-page/constants/index.ts
@@ -0,0 +1,11 @@
+export const initialValues = {
+  input: {
+    contractNumber: '',
+    contractDate: '',
+    firstName: '',
+    lastName: '',
+    middleName: undefined,
+    speciality: '',
+    grade: '',
+  },
+};
