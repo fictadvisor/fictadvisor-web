682a03eb434831e2c6e70565d636c19af9772973
Анастасія Лисенко <anastasiialysenko0@gmail.com>
2023-08-01 22:32:56 +0300

    Added all changes

--- src/components/pages/schedule-page/schedule-event-edit-section/components/schedule-input/ScheduleInput.styles.ts
+++ src/components/pages/schedule-page/schedule-event-edit-section/components/schedule-input/ScheduleInput.styles.ts
@@ -11,8 +11,23 @@ export const input = (size: ScheduleInputSize): SxProps<Theme> => ({
     '&::placeholder': {
       color: 'grey.600',
     },
-    '&.Mui-disabled': { '-webkit-text-fill-color': 'unset', opacity: 0.3 },
+    '&.Mui-disabled': {
+      '-webkit-text-fill-color': 'unset',
+      opacity: 0.3,
+      svg: {
+        fill: 'red',
+        '-webkit-text-fill-color': 'unset',
+        opacity: 0.3,
+      },
+    },
   },
+  '& .Mui-disabled': {
+    svg: {
+      '-webkit-text-fill-color': 'unset',
+      opacity: 0.3,
+    },
+  },
+
   '&:hover': {
     '& .MuiOutlinedInput-input': {
       '&::placeholder': {
@@ -25,6 +40,11 @@ export const input = (size: ScheduleInputSize): SxProps<Theme> => ({
     '& > fieldset': {
       border: 'none',
     },
+  },
+
+  svg: {
+    width: '18px',
+    height: '18px',
   },
 
   ...(size === ScheduleInputSize.NORMAL && {
