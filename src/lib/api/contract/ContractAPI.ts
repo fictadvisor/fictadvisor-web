417128bb05e03056a298b74a821c413e267a8fe1
igaryakqwe <igor.novykov@gmail.com>
2023-08-04 01:00:50 +0300

    fixed deployment

--- src/lib/api/contract/ContractAPI.ts
+++ src/lib/api/contract/ContractAPI.ts
@@ -1,14 +1,13 @@
 import {
   ContractBody,
   EntrantsPriorityBody,
+  PriorityData,
 } from '@/lib/api/contract/types/ContractBody';
 import { client } from '@/lib/api/instance';
 import { getAuthorizationHeader } from '@/lib/api/utils';
 
 import { AdminContractData } from './types/ContractBody';
 
-  PriorityData,
-} from '@/lib/api/contract/types/ContractBody';
 class ContractAPI {
   async createContract(body: ContractBody) {
     const { data } = await client.post('/documents/contract', body);
@@ -39,7 +38,7 @@ class ContractAPI {
       getAuthorizationHeader(),
     );
   }
-  
+
   async createPriority(body: PriorityData) {
     const { data } = await client.post('/documents/priority', body);
     return data;
