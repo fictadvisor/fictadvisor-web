8c81c4b25b172744ef3bf828b4b0288cdaf2d882
igaryakqwe <igor.novykov@gmail.com>
2023-08-10 15:45:55 +0300

    deleted extra interface

--- src/lib/api/contract/types/AdminContractBody.ts
+++ src/lib/api/contract/types/AdminContractBody.ts
@@ -1,8 +1,4 @@
-import { Fullname } from '@/types/contract';
-
-export interface PersonalEntrantData extends Fullname {
-  specialty: string;
-}
+import { EntrantBody } from '@/types/contract';
 
 export interface ContractAdminBody {
   number: string;
@@ -10,6 +6,6 @@ export interface ContractAdminBody {
 }
 
 export interface AdminContractBody {
-  entrant: PersonalEntrantData;
+  entrant: EntrantBody;
   contract: ContractAdminBody;
 }
