14378512614a0d2fb7680f1f12843941c2dc050a
anastasiia lysenko <anastasiialysenko0@gmail.com>
2023-08-10 23:15:50 +0300

    feature: schedule input added(#604)
    * Added some changes
    
    * Added all changes
    
    * Deleted test code

--- src/components/pages/schedule-page/schedule-event-edit-section/components/schedule-input/ScheduleInput.styles.ts
+++ src/components/pages/schedule-page/schedule-event-edit-section/components/schedule-input/ScheduleInput.styles.ts
@@ -7,17 +7,41 @@ export const input = (size: ScheduleInputSize): SxProps<Theme> => ({
   backgroundColor: 'backgroundDark.200',
   borderRadius: '8px',
   '& .MuiOutlinedInput-input': {
-    color: 'grey.600',
+    color: 'white.main',
     '&::placeholder': {
       color: 'grey.600',
     },
-    '&.Mui-disabled': { '-webkit-text-fill-color': 'unset', opacity: 0.3 },
+    '&.Mui-disabled': {
+      '-webkit-text-fill-color': 'unset',
+      opacity: 0.3,
+    },
   },
+  '& .Mui-disabled': {
+    svg: {
+      '-webkit-text-fill-color': 'unset',
+      opacity: 0.3,
+    },
+  },
+
+  '&:hover': {
+    '& .MuiOutlinedInput-input': {
+      '&::placeholder': {
+        color: 'grey.700',
+      },
+    },
+  },
+
   '& .MuiOutlinedInput-root': {
     '& > fieldset': {
       border: 'none',
     },
   },
+
+  svg: {
+    width: '18px',
+    height: '18px',
+  },
+
   ...(size === ScheduleInputSize.NORMAL && {
     '.MuiInputBase-root': {
       paddingY: '10px',
@@ -32,6 +56,7 @@ export const input = (size: ScheduleInputSize): SxProps<Theme> => ({
     '.MuiInputBase-root': {
       padding: '12px',
       minHeight: '50px',
+      fontSize: '20px',
     },
   }),
 });
