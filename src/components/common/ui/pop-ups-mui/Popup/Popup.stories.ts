0aaff53945cb1e29c35144043fd85a0b1fbec097
akiyama_kiyoshi <108092986+Ak1yamaKiyoshi@users.noreply.github.com>
2023-08-13 21:45:34 +0300

    documentation/popup
    * added buttons and icon to popup story  book

--- src/components/common/ui/pop-ups-mui/Popup/Popup.stories.ts
+++ src/components/common/ui/pop-ups-mui/Popup/Popup.stories.ts
@@ -4,7 +4,8 @@ import type { Meta, StoryObj } from '@storybook/react';
 import Popup from '@/components/common/ui/pop-ups-mui/Popup';
 import Button from '@/components/common/ui/button-mui/Button';
 import React from "react";
-import {ButtonColor, ButtonSize, ButtonVariant} from "@/components/common/ui/button-mui/types";
+import { LockClosed } from '@/components/common/icons/LockClosed';
+
 
 const meta = {
   title: 'Ui Kit/Components/Popup',
@@ -19,20 +20,17 @@ export default meta;
 
 type Story = StoryObj<typeof meta>;
 
-
-
 export const Base: Story = {
   args: {
-    text: 'Some text',
-    title: 'Some title',
+    title: 'Application Received ',
+    text: 'This will remove all data relating to Alex. This action cannot be reversed. Deleted data can not be recovered.',
     open: false,
     contentLeft: undefined,
     sx: undefined,
     hasCross: false,
-    icon: undefined,
-    firstButton: React.createElement(Button),
-    secondButton: undefined,
+    icon: React.createElement(LockClosed),
+    firstButton: React.createElement(Button, { text: "Delete" }),
+    secondButton: React.createElement(Button, { text: "Post" }),
     onClose: () => {},
   },
 };
-
