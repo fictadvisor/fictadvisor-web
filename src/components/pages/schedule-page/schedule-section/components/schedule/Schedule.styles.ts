bec4278eb9909b0f75d8f16128c9fe5ea341f96f
Igor <114025058+igaryakqwe@users.noreply.github.com>
2023-08-14 14:24:18 +0300

    added schedule time

---
+++ src/components/pages/schedule-page/schedule-section/components/schedule/Schedule.styles.ts
@@ -0,0 +1,16 @@
+import { SxProps, Theme } from '@mui/material/styles';
+
+export const layout: SxProps<Theme> = {
+  display: 'flex',
+  flexDirection: 'row',
+  backgroundColor: 'transparent',
+  width: '1082px',
+  height: '1354px',
+};
+
+export const column: SxProps<Theme> = {
+  display: 'flex',
+  backgroundColor: 'purple',
+  width: '148px',
+  height: '1354px',
+};
