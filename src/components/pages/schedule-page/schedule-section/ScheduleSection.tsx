8a0968fbf22c7ccefc462ff4d9a420fb01d85f18
Igor <114025058+igaryakqwe@users.noreply.github.com>
2023-08-14 14:24:18 +0300

    added schedule time

--- src/components/pages/schedule-page/schedule-section/ScheduleSection.tsx
+++ src/components/pages/schedule-page/schedule-section/ScheduleSection.tsx
@@ -1,5 +1,6 @@
 import { Typography } from '@mui/material';
 
+import Schedule from '@/components/pages/schedule-page/schedule-section/components/schedule';
 import ScheduleHeader from '@/components/pages/schedule-page/schedule-section/components/schedule-header';
 
 import styles from './schedule-section.module.scss';
@@ -8,7 +9,7 @@ export const ScheduleSection = ({}) => {
   return (
     <section className={styles['schedule-section']}>
       <ScheduleHeader />
-      <Typography>I AM SCHEDULE</Typography>
+      <Schedule />
     </section>
   );
 };
--- src/components/pages/schedule-page/schedule-section/components/schedule/Schedule.tsx
+++ src/components/pages/schedule-page/schedule-section/components/schedule/Schedule.tsx
@@ -1,7 +1,23 @@
 import React from 'react';
+import { Box } from '@mui/material';
+
+import ScheduleTime from '@/components/pages/schedule-page/schedule-section/components/schedule-time';
+
+import * as styles from './Schedule.styles';
 
 const Schedule = () => {
-  return <div></div>;
+  return (
+    <Box sx={styles.layout}>
+      <ScheduleTime />
+      <Box sx={styles.column}>1</Box>
+      <Box sx={styles.column}>2</Box>
+      <Box sx={styles.column}>3</Box>
+      <Box sx={styles.column}>4</Box>
+      <Box sx={styles.column}>5</Box>
+      <Box sx={styles.column}>6</Box>
+      <Box sx={styles.column}>7</Box>
+    </Box>
+  );
 };
 
 export default Schedule;
