1a64bb5f9490c1d9c17af9aebae0ee87b4f0a36c
Igor <114025058+igaryakqwe@users.noreply.github.com>
2023-08-14 13:18:37 +0300

    added endpoints

--- src/lib/api/schedule/ScheduleAPI.ts
+++ src/lib/api/schedule/ScheduleAPI.ts
@@ -2,14 +2,7 @@ import { client } from '@/lib/api/instance';
 import { getAuthorizationHeader } from '@/lib/api/utils';
 import { Event, TDiscipline } from '@/types/schedule';
 class ScheduleAPI {
-  async getLessonsByGroup(
-    groupId: string,
-    week: number,
-    /*
-     * TODO: add this to params when the back is ready
-     * disciplineType:TDiscipline
-     * */
-  ) {
+  async getLessons(groupId: string, week: number) {
     const { data } = await client.get<Event[]>(
       `/schedule/groups/${groupId}/general`,
       {
@@ -17,6 +10,14 @@ class ScheduleAPI {
       },
     );
 
+    return data;
+  }
+
+  async getLessonsByGroup(groupId: string, week: number): Promise<Event[]> {
+    const { data } = await client.get(`schedule/groups/${groupId}/events`, {
+      ...getAuthorizationHeader(),
+      params: { week },
+    });
     return data;
   }
 }
