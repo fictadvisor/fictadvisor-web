2335f3c7221c3637c8a28639f9a403f3a73bfacf
Igor <114025058+igaryakqwe@users.noreply.github.com>
2023-08-15 00:27:32 +0300

    added header day bar and aside time bar

--- src/components/pages/schedule-page/schedule-section/components/schedule-header/ScheduleHeader.tsx
+++ src/components/pages/schedule-page/schedule-section/components/schedule-header/ScheduleHeader.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { ChevronLeftIcon, ChevronRightIcon } from '@heroicons/react/24/outline';
 import { Box, Typography } from '@mui/material';
 
@@ -8,35 +8,81 @@ import {
 } from '@/components/common/ui/icon-button';
 import IconButton from '@/components/common/ui/icon-button-mui';
 import { IconButtonSize } from '@/components/common/ui/icon-button-mui/types';
+import { useSchedule } from '@/store/useSchedule';
 
 import * as styles from './ScheduleHeader.styles';
 
 const ScheduleHeader = () => {
-  const [week, setWeek] = useState(1);
+  const { week, setWeek } = useSchedule(state => ({
+    week: state.week,
+    setWeek: state.setWeek,
+  }));
+  const [prevButton, setPrevButton] = useState(false);
+  const [nextButton, setNextButton] = useState(false);
   const month = 'Лютий';
+  const startWeek = new Date('2023-08-14T16:43:37.041Z');
+  const startWeekDate = new Date('2023-08-14T16:43:37.041Z').getDate();
+  const dayMapper = ['Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб', 'Нд'];
+  const columns = [];
 
-  const handleClick = () => (week == 1 ? setWeek(2) : setWeek(1));
+  const getDayName = (index: number) => {
+    return dayMapper[index - 1];
+  };
+
+  for (let i = 0; i < 7; i++) {
+    const isCurDay = startWeekDate + i === new Date().getDate();
+    columns.push(
+      <Box sx={styles.column} key={i}>
+        <Typography sx={styles.dayName(isCurDay)}>
+          {getDayName(startWeek.getDay() + i)}
+        </Typography>
+        <Typography sx={styles.dayNumber(isCurDay)}>
+          {startWeekDate + i}
+        </Typography>
+      </Box>,
+    );
+  }
+
+  const nextWeek = () => {
+    setWeek(week + 1);
+  };
+
+  const prevWeek = () => {
+    setWeek(week - 1);
+  };
+
+  useEffect(() => {
+    week === 1 ? setPrevButton(true) : setPrevButton(false);
+    week === 20 ? setNextButton(true) : setNextButton(false);
+  }, [week]);
 
   return (
     <Box sx={styles.wrapper}>
-      <Typography sx={styles.month}>{month}</Typography>
-      <IconButton
-        sx={styles.button}
-        size={IconButtonSize.LARGE}
-        shape={IconButtonShape.SQUARE}
-        color={IconButtonColor.TRANSPARENT}
-        icon={<ChevronLeftIcon />}
-        onClick={handleClick}
-      />
-      <Typography sx={styles.week}>{week} тиждень</Typography>
-      <IconButton
-        sx={styles.button}
-        size={IconButtonSize.LARGE}
-        shape={IconButtonShape.SQUARE}
-        color={IconButtonColor.TRANSPARENT}
-        icon={<ChevronRightIcon />}
-        onClick={handleClick}
-      />
+      <Box sx={styles.date}>
+        <Typography sx={styles.month}>{month}</Typography>
+        <Box sx={styles.weekWrapper}>
+          <IconButton
+            disabled={prevButton}
+            sx={styles.button}
+            size={IconButtonSize.LARGE}
+            shape={IconButtonShape.SQUARE}
+            color={IconButtonColor.TRANSPARENT}
+            icon={<ChevronLeftIcon />}
+            onClick={prevWeek}
+          />
+          <Typography sx={styles.week}>{week} тиждень</Typography>
+          <IconButton
+            disabled={nextButton}
+            sx={styles.button}
+            size={IconButtonSize.LARGE}
+            shape={IconButtonShape.SQUARE}
+            color={IconButtonColor.TRANSPARENT}
+            icon={<ChevronRightIcon />}
+            onClick={nextWeek}
+          />
+        </Box>
+      </Box>
+      <Box sx={styles.columns}>{columns.map(column => column)}</Box>
     </Box>
   );
 };
