e538d37eae60b1849cdb4db097f7462ae3eb29b2
Yevhenii Mynenko <68693183+Yevheniy2020@users.noreply.github.com>
2023-08-16 00:00:01 +0300

    enhancement: added just input and formik input as MUI component
    -wrote input-mui and input-mui-formik
    -delete immutable input
    -rewrote all immutabple input as mui input

--- src/components/pages/account-page/components/security-tab/SecurityTab.tsx
+++ src/components/pages/account-page/components/security-tab/SecurityTab.tsx
@@ -1,13 +1,12 @@
 import React from 'react';
 import { useRouter } from 'next/router';
 
-import Button from '@/components/common/ui/button';
-import {
+import Button, {
   ButtonColor,
   ButtonSize,
   ButtonVariant,
-} from '@/components/common/ui/button-mui/types';
-import ImmutableInput from '@/components/common/ui/immutable-input';
+} from '@/components/common/ui/button';
+import Input from '@/components/common/ui/form/input-mui';
 import ChangePasswordForm from '@/components/pages/account-page/components/security-tab/components/change-password-form';
 import useAuthentication from '@/hooks/use-authentication';
 import AuthService from '@/lib/services/auth';
@@ -40,8 +39,8 @@ const SecurityTab = () => {
         <div className={styles['button']}></div>
       </div>
       <div className={styles['user-information']}>
-        <ImmutableInput label="Юзернейм" value={user.username} />
-        <ImmutableInput label="Пошта" value={user.email} />
+        <Input label="Юзернейм" value={user.username} />
+        <Input label="Пошта" value={user.email} />
       </div>
       <div className={styles['division']}>
         <div className={styles['white']}></div>
