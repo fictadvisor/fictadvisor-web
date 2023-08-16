b5dbe3843f6c42e684999b7fa4491793d9bd6cb6
Igor <114025058+igaryakqwe@users.noreply.github.com>
2023-08-16 20:56:23 +0300

    added functionality to schedule header

--- src/components/pages/schedule-page/schedule-section/ScheduleSection.tsx
+++ src/components/pages/schedule-page/schedule-section/ScheduleSection.tsx
@@ -1,11 +1,30 @@
-import { Typography } from '@mui/material';
+import { useEffect } from 'react';
 
+import Progress from '@/components/common/ui/progress-mui';
 import Schedule from '@/components/pages/schedule-page/schedule-section/components/schedule';
 import ScheduleHeader from '@/components/pages/schedule-page/schedule-section/components/schedule-header';
+import scheduleService from '@/lib/api/schedule/ScheduleAPI';
+import { useSchedule } from '@/store/useSchedule';
 
 import styles from './schedule-section.module.scss';
 
 export const ScheduleSection = ({}) => {
+  const groupId = '55b537e3-6786-454c-b934-3b92c7242095';
+  const week = useSchedule(state => state.week);
+  const setEventsBody = useSchedule(state => state.setEventsBody);
+  const eventsBody = useSchedule(state => state.eventsBody);
+  useEffect(() => {
+    const getEvents = async () => {
+      const res = await scheduleService.getEvents(groupId, week);
+      console.log(res);
+      setEventsBody(res);
+    };
+
+    getEvents();
+  }, [setEventsBody, week]);
+
+  if (!eventsBody) return <Progress />;
+
   return (
     <section className={styles['schedule-section']}>
       <ScheduleHeader />
