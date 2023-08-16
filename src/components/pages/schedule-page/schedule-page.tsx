eb2a555a039a56005ca1a7a23970124baa3722e1
kujo205 <kuc8301@gmail.com>
2023-08-16 16:28:22 +0300

    feature:added SSR to schedule and query parsing

--- src/components/pages/schedule-page/schedule-page.tsx
+++ src/components/pages/schedule-page/schedule-page.tsx
@@ -1,32 +1,65 @@
-import { useEffect } from 'react';
+import { FC, useEffect } from 'react';
 import { useRouter } from 'next/router';
 
 import PageLayout from '@/components/common/layout/page-layout/PageLayout';
+import { getCurrentWeek } from '@/components/pages/schedule-page/utils/getCurrentWeek';
+import { GetCurrentSemester } from '@/lib/api/dates/types/GetCurrentSemester';
 import { useSchedule } from '@/store/useSchedule';
+import { Group } from '@/types/group';
 
 import { CalendarSection } from './calendar-section/CalendarSection';
 import { ScheduleSection } from './schedule-section/ScheduleSection';
 
 import styles from './schedule-page.module.scss';
+const MAX_WEEK_NUMBER = 20;
+export interface SchedulePageProps {
+  groups: Group[];
+  semester: GetCurrentSemester | null;
+}
 
-const SchedulePage = () => {
+const SchedulePage: FC<SchedulePageProps> = ({ semester, groups }) => {
   const router = useRouter();
-  const { setGroupId, setWeek, setDate } = useSchedule(state => ({
+  const { setGroupId, setWeek, setDate, setChosenDay } = useSchedule(state => ({
     setGroupId: state.setGroupId,
     setWeek: state.setWeek,
     setDate: state.setDate,
+    setChosenDay: state.setChosenDay,
   }));
 
   useEffect(() => {
-    setInterval(() => {
+    const interval = setInterval(() => {
       setDate(new Date());
     }, 1000 * 60);
+
+    return () => clearInterval(interval);
   }, []);
 
   useEffect(() => {
-    if (!router.isReady) return;
+    if (!router.isReady || !semester) return;
     const { group, week } = router.query;
-    if (group && week && typeof group === 'string') {
+
+    if (
+      groups.every(_group => _group.id !== group) ||
+      (week && +week < 1) ||
+      (week && +week > MAX_WEEK_NUMBER)
+    ) {
+      router.push('/schedule');
+
+      const currentWeek = getCurrentWeek(semester);
+
+      let week = currentWeek;
+
+      if (currentWeek > MAX_WEEK_NUMBER) {
+        week = MAX_WEEK_NUMBER;
+        setChosenDay(new Date(semester.endDate));
+      }
+
+      setChosenDay(new Date());
+      setWeek(week);
+      return;
+    }
+
+    if (group && !Array.isArray(group) && week) {
       setGroupId(group);
       setWeek(+week);
     }
