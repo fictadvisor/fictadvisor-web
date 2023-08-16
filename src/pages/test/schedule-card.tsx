db6aee47d84c4492e965f36608764a5fe5ad8c5d
Igor <114025058+igaryakqwe@users.noreply.github.com>
2023-08-16 21:21:56 +0300

    deleted extra pages

--- src/pages/test/schedule-card.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-import ScheduleCardPage from '@/components/pages/test-pages/schedule-card-page';
-
-const ScheduleCard = () => {
-  return <ScheduleCardPage />;
-};
-
-export default ScheduleCard;
