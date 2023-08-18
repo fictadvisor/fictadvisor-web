2e73c21ffc43cbde99ebfcb64303b5730ca5b9f3
Yevhenii Mynenko <68693183+Yevheniy2020@users.noreply.github.com>
2023-08-18 16:07:12 +0300

    enhancement: review and SB
    -added StoryBook
    -fixed bugs
    -delete legacy test component

---
+++ src/components/common/ui/form/input-mui/Input.stories.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import type { Meta, StoryObj } from '@storybook/react';
+import { Form, Formik } from 'formik';
+import * as yup from 'yup';
+
+import Button from '@/components/common/ui/button-mui';
+import FormikInput from '@/components/common/ui/form/with-formik/input';
+
+const meta = {
+  title: 'Ui Kit/Components/Form/Formik/Input',
+  component: FormikInput,
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+} as Meta<typeof FormikInput>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Base: Story = {
+  render: args => (
+    <Formik
+      initialValues={{ t1: '', t2: '', t3: '', t4: '', t5: '', t6: '' }}
+      validationSchema={yup.object().shape({
+        t5: yup.string().required('Cannot be empty!'),
+      })}
+      onSubmit={(data, { setSubmitting }) => {
+        console.log(data);
+        setSubmitting(false);
+      }}
+    >
+      {({ handleSubmit }) => (
+        <Form>
+          <FormikInput {...args} />
+          <Button onClick={() => handleSubmit()} text="submit" />
+        </Form>
+      )}
+    </Formik>
+  ),
+  args: {
+    disabled: false,
+    readOnly: false,
+    name: 't5',
+  },
+};
