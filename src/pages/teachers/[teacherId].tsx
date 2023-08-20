8bba029e1c33836f54f858ff79fa72b7585c3cff
Yevhenii Mynenko <68693183+Yevheniy2020@users.noreply.github.com>
2023-08-20 19:24:12 +0300

    feature: added ISR

--- src/pages/teachers/[teacherId].tsx
+++ src/pages/teachers/[teacherId].tsx
@@ -5,7 +5,27 @@ import { useRouter } from 'next/router';
 import PageLayout from '@/components/common/layout/page-layout/PageLayout';
 import PersonalTeacherPage from '@/components/pages/personal-teacher-page';
 import useAuthentication from '@/hooks/use-authentication';
+import { client } from '@/lib/api/instance';
 import TeacherService from '@/lib/services/teacher/TeacherService';
+
+export async function getStaticPaths() {
+  const { data } = await client.get(`/teachers`);
+  const { teachers } = data;
+  const paths = teachers.map((teacher: { id: number }) => ({
+    params: { teacherId: teacher.id },
+  }));
+
+  return { paths, fallback: 'blocking' };
+}
+export async function getStaticProps() {
+  const { data } = await client.get(`/teachers`);
+  return {
+    props: {
+      data,
+    },
+    revalidate: 10,
+  };
+}
 
 const PersonalTeacher: FC = () => {
   const router = useRouter();
