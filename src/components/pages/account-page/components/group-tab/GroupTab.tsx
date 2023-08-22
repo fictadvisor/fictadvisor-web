ec01f61127c1a31643115907354b68ed63661908
ria4232 <ipkovalenomaria@gmail.com>
2023-08-22 21:38:37 +0300

    fix: done smth hope it worked

--- src/components/pages/account-page/components/group-tab/GroupTab.tsx
+++ src/components/pages/account-page/components/group-tab/GroupTab.tsx
@@ -1,97 +1,70 @@
-import { FC } from 'react';
-import { useQuery } from 'react-query';
-import { Box } from '@mui/material';
+import React, { FC, useState } from 'react';
+import { PencilIcon } from '@heroicons/react/24/outline';
+import { Avatar, Box, useMediaQuery } from '@mui/material';
+import { useRouter } from 'next/router';
 
-import Progress from '@/components/common/ui/progress';
-import NoGroupBlock from '@/components/pages/account-page/components/group-tab/components/no-group-block';
-import RequestsTable from '@/components/pages/account-page/components/group-tab/components/table/requests-table';
-import StudentsTable from '@/components/pages/account-page/components/group-tab/components/table/student-table';
+import { TelegramForAccount } from '@/components/common/icons/TelegramForAccount';
+import Button from '@/components/common/ui/button-mui';
 import {
-  transformRequestsData,
-  transformStudentsData,
-} from '@/components/pages/account-page/components/group-tab/components/table/utils';
+  ButtonSize,
+  ButtonVariant,
+} from '@/components/common/ui/button-mui/types';
+import Divider from '@/components/common/ui/divider';
+import { DividerTextAlign } from '@/components/common/ui/divider/types';
+import ChangeAvatarWindow from '@/components/pages/account-page/components/general-tab/components/change-avatar-window';
+import ContactsBlock from '@/components/pages/account-page/components/general-tab/components/contacts-block/ContactsBlock';
+import PersonalInfoBlock from '@/components/pages/account-page/components/general-tab/components/personal-info';
+import * as stylesMui from '@/components/pages/account-page/components/general-tab/GeneralTab.styles';
 import useAuthentication from '@/hooks/use-authentication';
-import GroupAPI from '@/lib/api/group/GroupAPI';
-import { PendingStudent } from '@/types/student';
-import { User, UserGroupRole, UserGroupState } from '@/types/user';
+import AuthService from '@/lib/services/auth';
+import theme from '@/styles/theme';
 
-const getStudents = async (user: User) => {
-  const { students } = await GroupAPI.getGroupStudents(
-    user.group?.id as string,
-  );
-  let requests: PendingStudent[] = [];
-
-  if (user.group?.role !== UserGroupRole.STUDENT) {
-    const { students: pendingStudents } = await GroupAPI.getRequestStudents(
-      user.group?.id as string,
-    );
-
-    requests = pendingStudents;
-  }
-
-  return {
-    students,
-    requests,
-  };
-};
-
-const GroupTab: FC = () => {
+const GeneralTab: FC = () => {
   const { user } = useAuthentication();
+  const router = useRouter();
+  const isMobile = useMediaQuery(theme.breakpoints.down('desktopSemiMedium'));
+  const buttonText = user.telegramId
+    ? 'Telegram під’єднано'
+    : "Під'єднати Telegram";
+  const [popupOpen, setPopupOpen] = useState(false);
 
-  const { data, isLoading, refetch } = useQuery(
-    ['students'],
-    () => getStudents(user),
-    {
-      retry: false,
-      refetchOnWindowFocus: false,
-    },
-  );
-
-  if (isLoading)
-    return (
-      <Box
-        sx={{
-          //TODO move inline styles when refactor
-          display: 'flex',
-          justifyContent: 'center',
-          alignItems: 'center',
-        }}
-      >
-        <Progress />
-      </Box>
-    );
-
-  if (
-    user?.group?.state === UserGroupState.DECLINED ||
-    user?.group?.state === UserGroupState.PENDING
-  )
-    return <NoGroupBlock />;
-
-  if (!data || !user?.group || !user?.group.role) return null;
-
-  const showRequests =
-    data?.requests?.length !== 0 && user?.group?.role !== UserGroupRole.STUDENT;
+  const handleConnectTelegram = () => {
+    void AuthService.redirectToRegisterBot(router);
+  };
 
   return (
-    <div>
-      <div>
-        <h4>Список групи {user.group.code}</h4>
-      </div>
-      {showRequests && (
-        <RequestsTable
-          refetch={refetch}
-          rows={transformRequestsData(data.requests)}
+    <Box sx={stylesMui.container}>
+      <Box sx={stylesMui.personalInfo}>
+        <PersonalInfoBlock />
+        <Divider
+          sx={stylesMui.divider}
+          textAlign={DividerTextAlign.LEFT}
+          text="Посилання на соц. мережі"
         />
+        <ContactsBlock />
+      </Box>
+      <Box sx={stylesMui.avatarAndTelegramInfo}>
+        <Box onClick={() => setPopupOpen(true)} sx={stylesMui.avatar}>
+          <Avatar src={user.avatar} alt="Фото профілю" sx={stylesMui.avatar} />
+          <Box>
+            <PencilIcon />
+          </Box>
+        </Box>
+        <Button
+          sx={stylesMui.telegramButton}
+          text={buttonText}
+          disabled={!!user.telegramId}
+          size={isMobile ? ButtonSize.SMALL : ButtonSize.MEDIUM}
+          startIcon={<TelegramForAccount />}
+          variant={ButtonVariant.OUTLINE}
+          onClick={handleConnectTelegram}
+        />
+      </Box>
+      {popupOpen && (
+        <ChangeAvatarWindow setPopupOpen={setPopupOpen} userId={user.id} />
       )}
-      {
-        <StudentsTable
-          refetch={refetch}
-          role={user.group.role}
-          rows={transformStudentsData(data.students)}
-        />
-      }
-    </div>
+    </Box>
   );
 };
 
-export default GroupTab;
+export default GeneralTab;
