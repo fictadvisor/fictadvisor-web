43032bbd1651b451c938db5677d317a11473b4b8
kujo205 <kuc8301@gmail.com>
2023-08-28 13:47:04 +0300

    bugfix: deleted test pages

--- src/pages/test/schedule-edit.tsx
+++
@@ -1,7 +0,0 @@
-import ScheduleEventSectionPage from '@/components/pages/test-pages/schedule-event-section-page/ScheduleEventSectionPage';
-
-const ScheduleEdit = () => {
-  return <ScheduleEventSectionPage />;
-};
-
-export default ScheduleEdit;
--- src/pages/test/schedule-info-card.tsx
+++
@@ -1,8 +0,0 @@
-import ScheduleInfoCard from '@/components/pages/schedule-page/schedule-event-edit-section/components/schedule-info-card';
-import { ScheduleEventEditDevice } from '@/components/pages/schedule-page/schedule-event-edit-section/types';
-
-const ScheduleInfoPage = () => {
-  return <ScheduleInfoCard week="1" device={ScheduleEventEditDevice.DESKTOP} />;
-};
-
-export default ScheduleInfoPage;
