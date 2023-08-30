ef938132118005ca7a67ca0b622f1c94b0663fe4
ria4232 <ipkovalenomaria@gmail.com>
2023-08-30 19:49:59 +0300

    fix: fixed smth

--- src/components/pages/account-page/components/security-tab/SecurityTab.tsx
+++ src/components/pages/account-page/components/security-tab/SecurityTab.tsx
@@ -1,76 +1,67 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
+import { Box, useMediaQuery } from '@mui/material';
 import { useRouter } from 'next/router';
 
-import Button from '@/components/common/ui/button';
+import Button from '@/components/common/ui/button-mui';
 import {
   ButtonColor,
   ButtonSize,
   ButtonVariant,
 } from '@/components/common/ui/button-mui/types';
-import Input from '@/components/common/ui/form/input-mui';
+import Divider from '@/components/common/ui/divider';
+import { DividerTextAlign } from '@/components/common/ui/divider/types';
+import ImmutableInput from '@/components/common/ui/immutable-input';
 import ChangePasswordForm from '@/components/pages/account-page/components/security-tab/components/change-password-form';
 import useAuthentication from '@/hooks/use-authentication';
 import AuthService from '@/lib/services/auth';
+import { hideAlert } from '@/redux/reducers/alert.reducer';
+import theme from '@/styles/theme';
 
-import styles from './SecurityTab.module.scss';
+import * as styles from './SecurityTab.styles';
 
 const SecurityTab = () => {
   const { replace, reload } = useRouter();
   const { user, update } = useAuthentication();
+  const dispatch = useDispatch();
   const handleLogout = async () => {
     await AuthService.logout();
     update();
     reload();
     await replace('/login');
+    dispatch(hideAlert());
   };
+  const isMobile = useMediaQuery(theme.breakpoints.down('desktopSemiMedium'));
 
   return (
-    <div className={styles['container']}>
-      <div className={styles['division']}>
-        <h4 className={styles['division-text']}>Зміна паролю</h4>
-        <div className={styles['white']}></div>
-        <div className={styles['button']}></div>
-      </div>
-      <div className={styles['input-form']}>
+    <Box sx={styles.container}>
+      <Divider
+        text={'Зміна паролю'}
+        textAlign={DividerTextAlign.LEFT}
+      ></Divider>
+      <Box sx={styles.inputForm}>
         <ChangePasswordForm />
-      </div>
-      <div className={styles['division']}>
-        <h4 className={styles['division-text']}>Юзернейм і пошта</h4>
-        <div className={styles['white']}></div>
-        <div className={styles['button']}></div>
-      </div>
-      <div className={styles['user-information']}>
-        <Input
-          readOnly
-          onChange={() => {}}
-          label="Юзернейм"
-          value={user.username}
-        />
-        <Input readOnly onChange={() => {}} label="Пошта" value={user.email} />
-      </div>
-      <div className={styles['division']}>
-        <div className={styles['white']}></div>
-        <div className={styles['button']}></div>
-      </div>
-      <div className={styles['button-container']}>
+      </Box>
+      <Divider
+        text={'Юзернейм і пошта'}
+        textAlign={DividerTextAlign.LEFT}
+      ></Divider>
+
+      <Box sx={styles.userInformation}>
+        <ImmutableInput label="Юзернейм" value={user.username} />
+        <ImmutableInput label="Пошта" value={user.email} />
+      </Box>
+      <Divider></Divider>
+      <Box sx={styles.buttonContainer}>
         <Button
           text={'Вийти з акаунту'}
           variant={ButtonVariant.FILLED}
           color={ButtonColor.SECONDARY}
-          size={ButtonSize.MEDIUM}
+          size={isMobile ? ButtonSize.SMALL : ButtonSize.MEDIUM}
           onClick={handleLogout}
         />
-      </div>
-      <div className={styles['button-container-mobile']}>
-        <Button
-          text={'Вийти з акаунту'}
-          variant={ButtonVariant.FILLED}
-          color={ButtonColor.SECONDARY}
-          size={ButtonSize.SMALL}
-          onClick={handleLogout}
-        />
-      </div>
-    </div>
+      </Box>
+    </Box>
   );
 };
 
