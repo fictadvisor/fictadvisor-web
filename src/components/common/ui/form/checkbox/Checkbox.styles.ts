bfa32a8ede8f9760938822d08fe721bc221fec2a
yablonya <yablonskyi.danylo@lll.kpi.ua>
2023-09-01 15:21:40 +0300

    bugfix: fixed register page layout on small screen and remove commented code in LoginFormBlock.styles.ts

--- src/components/common/ui/form/checkbox/Checkbox.styles.ts
+++ src/components/common/ui/form/checkbox/Checkbox.styles.ts
@@ -12,5 +12,9 @@ export const checkBox: SxProps<Theme> = {
 export const label = (disabled: boolean, label?: string): SxProps<Theme> => ({
   color: disabled ? 'grey.400' : 'grey.800',
   marginLeft: label && '8px',
-  whiteSpace: 'nowrap',
+  minWidth: '75px',
+  whiteSpace: {
+    mobileSemiMedium: 'normal',
+    mobileMedium: 'nowrap',
+  },
 });
