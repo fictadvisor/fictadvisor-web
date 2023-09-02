00db1edeb7a323d58ed37c2dc8931563e2d2de64
Markusplay <mshilo2004@gmail.com>
2023-09-02 20:50:07 +0300

    added `Tooltip`

---
+++ v3/components/tooltip/Tooltip.tsx
@@ -0,0 +1,24 @@
+import { FC, ReactElement } from 'react';
+import { Tooltip as MuiTooltip, TooltipProps } from '@mui/material';
+
+import * as styles from './Tooltip.styles';
+
+interface StyledTooltipProps extends TooltipProps {
+  children: ReactElement;
+}
+
+const Tooltip: FC<StyledTooltipProps> = ({ children, ...rest }) => {
+  return (
+    <MuiTooltip
+      {...rest}
+      slotProps={{
+        tooltip: { sx: styles.tooltip },
+        arrow: { sx: styles.arrow },
+      }}
+    >
+      {children}
+    </MuiTooltip>
+  );
+};
+
+export default Tooltip;
