4ce6ba0b654ab860d73a9638cbfaa3cfea1e7d50
ria4232 <ipkovalenomaria@gmail.com>
2023-09-02 21:21:02 +0300

    fix: fixed smthdsadsafd

--- src/components/pages/account-page/components/general-tab/components/personal-info/PersonalInfo.tsx
+++ src/components/pages/account-page/components/general-tab/components/personal-info/PersonalInfo.tsx
@@ -1,8 +1,10 @@
 import React, { FC } from 'react';
+import { shallowEqual } from 'react-redux';
+import { Box, useMediaQuery } from '@mui/material';
 import { Form, Formik } from 'formik';
 
 import { CustomCheck } from '@/components/common/icons/CustomCheck';
-import Button from '@/components/common/ui/button';
+import Button from '@/components/common/ui/button-mui';
 import { ButtonSize } from '@/components/common/ui/button-mui/types';
 import { Input } from '@/components/common/ui/form';
 import { PersonalInfoForm } from '@/components/pages/account-page/components/general-tab/components/personal-info/types';
@@ -11,12 +13,12 @@ import useAuthentication from '@/hooks/use-authentication';
 import useToast from '@/hooks/use-toast';
 import UserAPI from '@/lib/api/user/UserAPI';
 import getErrorMessage from '@/lib/utils/getErrorMessage';
+import theme from '@/styles/theme';
 
-import styles from '../../GeneralTab.module.scss';
+import * as styles from '../../GeneralTab.styles';
 
-const objectsEqual = (obj1: object, obj2: object) => {
-  return JSON.stringify(obj1) === JSON.stringify(obj2);
-};
+import stylesFUCK from '../../GeneralTab.module.scss';
+
 const PersonalInfoBlock: FC = () => {
   const { user, update } = useAuthentication();
   const initialValues: PersonalInfoForm = {
@@ -28,25 +30,20 @@ const PersonalInfoBlock: FC = () => {
   const toast = useToast();
 
   const handleSubmit = async (data: PersonalInfoForm) => {
-    data.firstName = data.firstName.trim().replace('`', `'`).replace('ʼ', `'`);
-    data.middleName = data.middleName
-      ?.trim()
-      .replace('`', `'`)
-      .replace('ʼ', `'`);
-    data.lastName = data.lastName.trim().replace('`', `'`).replace('ʼ', `'`);
+    data.firstName = data.firstName.trim().replace(/[`ʼ]/g, "'");
+    data.middleName = data.middleName?.trim().replace(/[`ʼ]/g, "'");
+    data.lastName = data.lastName.trim().replace(/[`ʼ]/g, "'");
 
     if (!data.middleName) delete data.middleName;
 
     try {
       await UserAPI.changeInfo(user.id, data);
       await update();
-    } catch (error) {
-      const message = getErrorMessage(error);
-      message
-        ? toast.error(message)
-        : toast.error('Щось пішло не так, спробуй пізніше!');
+    } catch (e) {
+      toast.error(getErrorMessage(e));
     }
   };
+  const isMobile = useMediaQuery(theme.breakpoints.down('desktopSemiMedium'));
 
   return (
     <>
@@ -57,38 +54,32 @@ const PersonalInfoBlock: FC = () => {
         validationSchema={validationSchema}
       >
         {({ isValid, values }) => (
-          <Form className={styles['form']}>
+          <Form className={stylesFUCK['form']}>
             <Input
-              className={styles['input']}
+              className={stylesFUCK['input']}
               label="Прізвище"
               name="lastName"
             />
-            <Input className={styles['input']} label="Ім'я" name="firstName" />
             <Input
-              className={styles['input']}
+              className={stylesFUCK['input']}
+              label="Ім'я"
+              name="firstName"
+            />
+            <Input
+              className={stylesFUCK['input']}
               label="По батькові"
               name="middleName"
             />
-            <div className={styles['confirm-button']}>
+            <Box sx={styles.confirmButton}>
               <Button
                 text="Зберегти зміни"
                 startIcon={<CustomCheck />}
-                size={ButtonSize.MEDIUM}
+                size={isMobile ? ButtonSize.SMALL : ButtonSize.MEDIUM}
                 type="submit"
-                disabled={!isValid || objectsEqual(initialValues, values)}
-                className={styles['change-password-button']}
+                disabled={!isValid || shallowEqual(initialValues, values)}
+                sx={isMobile ? { padding: '6px 12px' } : {}}
               />
-            </div>
-            <div className={styles['confirm-button-mobile']}>
-              <Button
-                text="Зберегти зміни"
-                startIcon={<CustomCheck />}
-                size={ButtonSize.SMALL}
-                type="submit"
-                disabled={!isValid || objectsEqual(initialValues, values)}
-                className={styles['change-password-button']}
-              />
-            </div>
+            </Box>
           </Form>
         )}
       </Formik>
