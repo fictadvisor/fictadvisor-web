be94cefbac55ced5e9b4801d86e3956f969674e1
Markusplay <mshilo2004@gmail.com>
2023-09-02 22:46:49 +0300

    added default styles

--- v3/app/layout.tsx
+++ v3/app/layout.tsx
@@ -1,7 +1,9 @@
 // import { Metadata } from 'next';
 
 // import { Inter } from 'next/font/google';
-import './globals.css';
+import '@/styles/reset.scss';
+import '@/styles/typography.scss';
+import '@/styles/global-styles.scss';
 
 // const inter = Inter({ subsets: ['latin'] });
 
