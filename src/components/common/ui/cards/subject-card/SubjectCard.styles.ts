f5365fc9896c608c07ea1a68289fed30d62db0d5
Yevhenii Mynenko <68693183+Yevheniy2020@users.noreply.github.com>
2023-09-03 16:38:07 +0300

    bugfix: fixed for safari

--- src/components/common/ui/cards/subject-card/SubjectCard.styles.ts
+++ src/components/common/ui/cards/subject-card/SubjectCard.styles.ts
@@ -13,6 +13,8 @@ export const subjectCard = (disabled: boolean): SxProps<Theme> => ({
   transition: 'all 0.2s ease-in-out',
   display: 'flex',
   flexDirection: 'column',
+  fontFeatureSettings: 'hyph 1',
+  WebkitHyphens: 'auto',
   hyphens: 'auto',
   wordWrap: 'break-word',
   gap: '8px',
