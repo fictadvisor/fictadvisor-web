0952ffad76426f1ace3457f64ecc25020c3e4271
yablonya <122230172+yablonya@users.noreply.github.com>
2023-09-15 23:40:49 +0300

    enhancement: rewrote QuestionsList, AnswersSheet and PollPage using mui (#693)
    * enhancement: rewrote QuestionsList.tsx and AnswersSheet.tsx using mui
    
    * enhancement: rewrote PollPage.tsx using mui
    
    * fucked poll page(to its normal state)

--- src/components/pages/poll-page/components/answers-sheet/AnswerSheet.style.ts
+++ src/components/pages/poll-page/components/answers-sheet/AnswerSheet.style.ts
@@ -1,26 +1,135 @@
 import { SxProps, Theme } from '@mui/material/styles';
 
-export const textArea: SxProps<Theme> = {
-  width: {
-    mobile: '100%',
-    desktop: '500px',
-    desktopMedium: '632px',
+import theme from '@/styles/theme';
+
+export const wrapper: SxProps<Theme> = {
+  width: '632px',
+  marginLeft: '124px',
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+
+  '@media screen and (max-width: 1600px)': {
+    maxWidth: '800px',
+    width: '800px',
   },
-  '& .MuiInputBase-root': {
-    height: {
-      mobile: '228px',
-      desktop: '120px',
+
+  '@media screen and (max-width: 1500px)': {
+    maxWidth: '700px',
+    width: '700px',
+  },
+
+  '@media screen and (max-width: 1400px)': {
+    maxWidth: '500px',
+  },
+
+  '@media screen and (max-width: 1280px)': {
+    width: '360px',
+  },
+
+  [theme.breakpoints.down('desktop')]: {
+    maxWidth: '100vw',
+    width: '100vw',
+    margin: 0,
+    animationName: '$appear',
+    animationDuration: '0.3s',
+  },
+
+  '@keyframes appear': {
+    from: {
+      opacity: 0,
+    },
+
+    to: {
+      opacity: 1,
     },
   },
 };
 
-export const chevronIcon: SxProps<Theme> = {
-  position: 'absolute',
-  left: {
-    mobile: '4px',
-    mobileSemiMedium: '16px',
-  },
+export const successWrapper: SxProps<Theme> = {
+  margin: 0,
+  display: 'flex',
   alignItems: 'center',
   justifyContent: 'center',
-  height: '20px',
+};
+
+export const answersWrapper: SxProps<Theme> = {
+  width: '100%',
+
+  [theme.breakpoints.down('desktop')]: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    margin: 0,
+    height: '100%',
+  },
+};
+
+export const loaderWrapper: SxProps<Theme> = {
+  width: '100%',
+  height: '100%',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+};
+
+export const answersSavedWrapper: SxProps<Theme> = {
+  minHeight: '100vh',
+  minWidth: '100%',
+  width: '100vw',
+  height: '100%',
+  margin: 0,
+};
+
+export const form: SxProps<Theme> = {
+  display: 'flex',
+  flexDirection: 'column',
+  gap: '40px',
+
+  [theme.breakpoints.down('desktop')]: {
+    display: 'flex',
+    flexDirection: 'column',
+    gap: '32px',
+    width: '100%',
+    height: '100%',
+    padding: '24px 16px',
+  },
+};
+
+export const toQuestionsList: SxProps<Theme> = {
+  width: '100%',
+  height: '52px',
+  borderBottom: `1px solid ${theme.palette.backgroundDark[400]}`,
+  transition: '0.3s',
+  padding: '0 15px',
+  display: 'none',
+
+  '&:hover': {
+    background: theme.palette.backgroundDark[300],
+  },
+  '&:active': {
+    background: theme.palette.backgroundDark[400],
+  },
+
+  [theme.breakpoints.down('desktop')]: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+};
+
+export const button: SxProps<Theme> = {
+  width: 'fit-content',
+  borderRadius: '8px',
+
+  [theme.breakpoints.down('desktop')]: {
+    width: '100%',
+    marginBottom: '38px',
+  },
+};
+
+export const questionName: SxProps<Theme> = {
+  width: 'fit-content',
+  fontSize: '14px',
+  fontWeight: 'bold',
 };
