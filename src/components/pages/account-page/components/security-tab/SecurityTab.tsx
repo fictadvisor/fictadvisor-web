53fb5bf80652af4065138947ea38240e5d15ea00
sifes <lonfark@gmail.com>
2023-11-03 12:40:59 +0200

    bugfix: changed all "ви" to "ти" form

--- src/components/pages/account-page/components/security-tab/SecurityTab.tsx
+++ src/components/pages/account-page/components/security-tab/SecurityTab.tsx
@@ -40,11 +40,11 @@ const SecurityTab = () => {
       <Box sx={styles.userInformation}>
         <Input
           readOnly
-          onChange={() => { }}
+          onChange={() => {}}
           label="Юзернейм"
           value={user.username}
         />
-        <Input readOnly onChange={() => { }} label="Пошта" value={user.email} />
+        <Input readOnly onChange={() => {}} label="Пошта" value={user.email} />
       </Box>
       <Divider sx={styles.divider} />
       <Button
