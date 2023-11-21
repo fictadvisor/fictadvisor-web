51ad6bbaea92d8e98baed7eedb8874b44a41d010
Markusplay <mshilo2004@gmail.com>
2023-11-22 01:22:26 +0200

    fixed useToast bug

--- src/app/register/email-verification/page.tsx
+++ src/app/register/email-verification/page.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import PageLayout from '@/components/common/layout/page-layout';
 import RegistrationEmailConfirmationPage from '@/components/pages/register/email-confirmation-page';
 
